fe6511a95dab0b459f8f47a106fe4f239b0b3ef6
srafi2 <srafi1@stuy.edu>
2017-10-16 12:17:25 -0400

    init

--- stu_mean.py
+++ stu_mean.py
@@ -5,3 +5,23 @@ Brian Leung
 HW 10: Average
 PD 7 SoftDev
 '''
+
+import sqlite3
+
+f = 'discobandit.db'
+db = sqlite3.connect(f)
+c = db.cursor()
+q = '''SELECT name, peeps.id, mark
+FROM peeps, courses
+WHERE peeps.id = courses.id;'''
+foo  = c.execute(q)
+
+students = {}
+for bar in foo:
+    name = bar[0]
+    if name in students:
+        students[name]['total'] += bar[2]
+        students[name]['num_classes'] += 1
+    else:
+        students[
+        
